e41a080e1804748b88ae6e438b1d58626b167046
dararith-coding <dararith-coding.noreply@github.com>
2020-07-01 10:40:21 +0700

    Updates

---
+++ precache-manifest.f96a36df0d759d5d3ac82723867e59b3.js
@@ -0,0 +1,94 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8211c9b8c206867eb1ae22c702b2ffbc",
+    "url": "/telegram-react/ebc9db11cd3500eeab4f.worker.js"
+  },
+  {
+    "revision": "982ee26e0c2226deecf64e3ca4262466",
+    "url": "/telegram-react/index.html"
+  },
+  {
+    "revision": "e7b081556abc8a3b1a19",
+    "url": "/telegram-react/static/css/0.39316fe9.chunk.css"
+  },
+  {
+    "revision": "a369d4e74fca9b3e5c97",
+    "url": "/telegram-react/static/css/4.3330a4e8.chunk.css"
+  },
+  {
+    "revision": "088b65c48327e5865839",
+    "url": "/telegram-react/static/css/6.6ede3073.chunk.css"
+  },
+  {
+    "revision": "166c996ce4dd24eaf672",
+    "url": "/telegram-react/static/css/8.213f2ea2.chunk.css"
+  },
+  {
+    "revision": "b91b83104f087b013546",
+    "url": "/telegram-react/static/css/main.88a1993f.chunk.css"
+  },
+  {
+    "revision": "e7b081556abc8a3b1a19",
+    "url": "/telegram-react/static/js/0.989bc0e3.chunk.js"
+  },
+  {
+    "revision": "b0946b626446445ad5df",
+    "url": "/telegram-react/static/js/1.dc7ea370.chunk.js"
+  },
+  {
+    "revision": "a369d4e74fca9b3e5c97",
+    "url": "/telegram-react/static/js/4.887a3259.chunk.js"
+  },
+  {
+    "revision": "cccea726f62e03f752d3",
+    "url": "/telegram-react/static/js/5.492ee296.chunk.js"
+  },
+  {
+    "revision": "7c6aa3f8ee6731ba3889fd8f8e268352",
+    "url": "/telegram-react/static/js/5.492ee296.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "088b65c48327e5865839",
+    "url": "/telegram-react/static/js/6.72efa5eb.chunk.js"
+  },
+  {
+    "revision": "2e4138f38b3e646298562484feee056e",
+    "url": "/telegram-react/static/js/6.72efa5eb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "78e5b99ca0bde67da22f",
+    "url": "/telegram-react/static/js/7.44cc3234.chunk.js"
+  },
+  {
+    "revision": "166c996ce4dd24eaf672",
+    "url": "/telegram-react/static/js/8.9c36f98d.chunk.js"
+  },
+  {
+    "revision": "b91b83104f087b013546",
+    "url": "/telegram-react/static/js/main.bbefd79e.chunk.js"
+  },
+  {
+    "revision": "5c9378e1d2cce10bf84d",
+    "url": "/telegram-react/static/js/runtime-main.374738f7.js"
+  },
+  {
+    "revision": "4e9d3194a07f7acbb8fd3a1098318113",
+    "url": "/telegram-react/static/media/bg.4e9d3194.jpg"
+  },
+  {
+    "revision": "cbb4eead04871188de6ce488808e4237",
+    "url": "/telegram-react/static/media/bubble-tail-left.cbb4eead.svg"
+  },
+  {
+    "revision": "b159f8b04a1646f68bfc0bfb7f347cac",
+    "url": "/telegram-react/static/media/bubble-tail-right.b159f8b0.svg"
+  },
+  {
+    "revision": "71da74694e314a0e3a855ded564a1eaf",
+    "url": "/telegram-react/static/media/check.71da7469.svg"
+  },
+  {
+    "revision": "ac1331a490a116a48daa6c9f41b6db80",
+    "url": "/telegram-react/static/media/telegram-logo.ac1331a4.svg"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,282 @@
+/**
+ * Copyright 2016 Google Inc. All rights reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+*/
+
+// DO NOT EDIT THIS GENERATED OUTPUT DIRECTLY!
+// This file should be overwritten as part of your build process.
+// If you need to extend the behavior of the generated service worker, the best approach is to write
+// additional code and include it using the importScripts option:
+//   https://github.com/GoogleChrome/sw-precache#importscripts-arraystring
+//
+// Alternatively, it's possible to make changes to the underlying template file and then use that as the
+// new base for generating output, via the templateFilePath option:
+//   https://github.com/GoogleChrome/sw-precache#templatefilepath-string
+//
+// If you go that route, make sure that whenever you update your sw-precache dependency, you reconcile any
+// changes made to this original template file with your modified copy.
+
+// This generated service worker JavaScript will precache your site's resources.
+// The code needs to be saved in a .js file at the top-level of your site, and registered
+// from your pages in order to be used. See
+// https://github.com/googlechrome/sw-precache/blob/master/demo/app/js/service-worker-registration.js
+// for an example of how you can register this script and handle various service worker events.
+
+/* eslint-env worker, serviceworker */
+/* eslint-disable indent, no-unused-vars, no-multiple-empty-lines, max-nested-callbacks, space-before-function-paren, quotes, comma-spacing */
+'use strict';
+
+var precacheConfig = [["/telegram-react/1.ead4614006a580316820.worker.js","c7d1db6edec8b0b0b62fa419b410048d"],["/telegram-react/93851361833a55534e17170c3e15da85.wasm","93851361833a55534e17170c3e15da85"],["/telegram-react/Manytabs_2x.png","7a71629a5e4f7482b6320b103f1d543c"],["/telegram-react/WP_2x.jpg","a1d37d509e6740f40c0a4729dff3100b"],["/telegram-react/data/TwoFactorSetupMonkeyClose.json","c322e3eabf46340f12ec7f4784d4115a"],["/telegram-react/data/TwoFactorSetupMonkeyIdle.json","b2c77121d458e17d18e642c51cb9821c"],["/telegram-react/data/TwoFactorSetupMonkeyPeek.json","d914e5d5fdef6b5596bb409689c8332d"],["/telegram-react/data/TwoFactorSetupMonkeyTracking.json","addf1beac01749387f1aea96a9bfb682"],["/telegram-react/data/countries.txt","ca17aa9eaa6afb376d594a5775c37b25"],["/telegram-react/ead4614006a580316820.worker.js","47e130c4818929c84df5470d9506b26b"],["/telegram-react/ebc9db11cd3500eeab4f.worker.js","8211c9b8c206867eb1ae22c702b2ffbc"],["/telegram-react/emoji-mart.dark.css","f86266262951b587e8fe819965076795"],["/telegram-react/emoji-mart.light.css","b96944920644feee9500bfca3815b35a"],["/telegram-react/iOS_2x.jpg","1d95f349db03f730edb3bc35224a303e"],["/telegram-react/index.html","982ee26e0c2226deecf64e3ca4262466"],["/telegram-react/sound_a.mp3","eba09b6a457792c52fc610b5f9f974b3"],["/telegram-react/static/css/0.39316fe9.chunk.css","1b36ed650d52e3c79443cafc6103f23e"],["/telegram-react/static/css/4.3330a4e8.chunk.css","650abe9211de94ec6d6af93345366337"],["/telegram-react/static/css/6.6ede3073.chunk.css","f5e06c5e3f5578b56300915125c7b7d9"],["/telegram-react/static/css/8.213f2ea2.chunk.css","7f6ac73687272ea76a9f47df72ae3bae"],["/telegram-react/static/css/main.88a1993f.chunk.css","81cd5127ba71332ea83be736cf0c3209"],["/telegram-react/static/js/0.989bc0e3.chunk.js","782b7d3456adcc54af001fac8afa5184"],["/telegram-react/static/js/1.dc7ea370.chunk.js","b672c535323631067e0e00945c6d2078"],["/telegram-react/static/js/4.887a3259.chunk.js","08ecca5f3c2f67bd556d85990c8367e0"],["/telegram-react/static/js/5.492ee296.chunk.js","2ba89d4f3fdad3025a0d04e11304d823"],["/telegram-react/static/js/6.72efa5eb.chunk.js","cd553638908c684275e0ebd16b6b5efb"],["/telegram-react/static/js/7.44cc3234.chunk.js","cc85bdea51ce2d5716f7622bb876b363"],["/telegram-react/static/js/8.9c36f98d.chunk.js","c5c42b26034e0386402b0a3a0236a1da"],["/telegram-react/static/js/main.bbefd79e.chunk.js","41acae0838cac1d01f6d0fa7fc43a4d9"],["/telegram-react/static/js/runtime-main.374738f7.js","001fb8d002dca7f86b555e7a40e50b45"],["/telegram-react/static/media/bg.4e9d3194.jpg","4e9d3194a07f7acbb8fd3a1098318113"],["/telegram-react/static/media/bubble-tail-left.cbb4eead.svg","cbb4eead04871188de6ce488808e4237"],["/telegram-react/static/media/bubble-tail-right.b159f8b0.svg","b159f8b04a1646f68bfc0bfb7f347cac"],["/telegram-react/static/media/check.71da7469.svg","71da74694e314a0e3a855ded564a1eaf"],["/telegram-react/static/media/telegram-logo.ac1331a4.svg","ac1331a490a116a48daa6c9f41b6db80"],["/telegram-react/tdweb.js","64b78979a9381a3293d34f5be9d9e719"]];
+var cacheName = 'sw-precache-v3-sw-precache-webpack-plugin-' + (self.registration ? self.registration.scope : '');
+
+
+var ignoreUrlParametersMatching = [/./];
+
+
+
+var addDirectoryIndex = function(originalUrl, index) {
+    var url = new URL(originalUrl);
+    if (url.pathname.slice(-1) === '/') {
+      url.pathname += index;
+    }
+    return url.toString();
+  };
+
+var cleanResponse = function(originalResponse) {
+    // If this is not a redirected response, then we don't have to do anything.
+    if (!originalResponse.redirected) {
+      return Promise.resolve(originalResponse);
+    }
+
+    // Firefox 50 and below doesn't support the Response.body stream, so we may
+    // need to read the entire body to memory as a Blob.
+    var bodyPromise = 'body' in originalResponse ?
+      Promise.resolve(originalResponse.body) :
+      originalResponse.blob();
+
+    return bodyPromise.then(function(body) {
+      // new Response() is happy when passed either a stream or a Blob.
+      return new Response(body, {
+        headers: originalResponse.headers,
+        status: originalResponse.status,
+        statusText: originalResponse.statusText
+      });
+    });
+  };
+
+var createCacheKey = function(originalUrl, paramName, paramValue,
+                           dontCacheBustUrlsMatching) {
+    // Create a new URL object to avoid modifying originalUrl.
+    var url = new URL(originalUrl);
+
+    // If dontCacheBustUrlsMatching is not set, or if we don't have a match,
+    // then add in the extra cache-busting URL parameter.
+    if (!dontCacheBustUrlsMatching ||
+        !(url.pathname.match(dontCacheBustUrlsMatching))) {
+      url.search += (url.search ? '&' : '') +
+        encodeURIComponent(paramName) + '=' + encodeURIComponent(paramValue);
+    }
+
+    return url.toString();
+  };
+
+var isPathWhitelisted = function(whitelist, absoluteUrlString) {
+    // If the whitelist is empty, then consider all URLs to be whitelisted.
+    if (whitelist.length === 0) {
+      return true;
+    }
+
+    // Otherwise compare each path regex to the path of the URL passed in.
+    var path = (new URL(absoluteUrlString)).pathname;
+    return whitelist.some(function(whitelistedPathRegex) {
+      return path.match(whitelistedPathRegex);
+    });
+  };
+
+var stripIgnoredUrlParameters = function(originalUrl,
+    ignoreUrlParametersMatching) {
+    var url = new URL(originalUrl);
+    // Remove the hash; see https://github.com/GoogleChrome/sw-precache/issues/290
+    url.hash = '';
+
+    url.search = url.search.slice(1) // Exclude initial '?'
+      .split('&') // Split into an array of 'key=value' strings
+      .map(function(kv) {
+        return kv.split('='); // Split each 'key=value' string into a [key, value] array
+      })
+      .filter(function(kv) {
+        return ignoreUrlParametersMatching.every(function(ignoredRegex) {
+          return !ignoredRegex.test(kv[0]); // Return true iff the key doesn't match any of the regexes.
+        });
+      })
+      .map(function(kv) {
+        return kv.join('='); // Join each [key, value] array into a 'key=value' string
+      })
+      .join('&'); // Join the array of 'key=value' strings into a string with '&' in between each
+
+    return url.toString();
+  };
+
+
+var hashParamName = '_sw-precache';
+var urlsToCacheKeys = new Map(
+  precacheConfig.map(function(item) {
+    var relativeUrl = item[0];
+    var hash = item[1];
+    var absoluteUrl = new URL(relativeUrl, self.location);
+    var cacheKey = createCacheKey(absoluteUrl, hashParamName, hash, /\.\w{8}\./);
+    return [absoluteUrl.toString(), cacheKey];
+  })
+);
+
+function setOfCachedUrls(cache) {
+  return cache.keys().then(function(requests) {
+    return requests.map(function(request) {
+      return request.url;
+    });
+  }).then(function(urls) {
+    return new Set(urls);
+  });
+}
+
+self.addEventListener('install', function(event) {
+  event.waitUntil(
+    caches.open(cacheName).then(function(cache) {
+      return setOfCachedUrls(cache).then(function(cachedUrls) {
+        return Promise.all(
+          Array.from(urlsToCacheKeys.values()).map(function(cacheKey) {
+            // If we don't have a key matching url in the cache already, add it.
+            if (!cachedUrls.has(cacheKey)) {
+              var request = new Request(cacheKey, {credentials: 'same-origin'});
+              return fetch(request).then(function(response) {
+                // Bail out of installation unless we get back a 200 OK for
+                // every request.
+                if (!response.ok) {
+                  throw new Error('Request for ' + cacheKey + ' returned a ' +
+                    'response with status ' + response.status);
+                }
+
+                return cleanResponse(response).then(function(responseToCache) {
+                  return cache.put(cacheKey, responseToCache);
+                });
+              });
+            }
+          })
+        );
+      });
+    }).then(function() {
+      
+      // Force the SW to transition from installing -> active state
+      return self.skipWaiting();
+      
+    })
+  );
+});
+
+self.addEventListener('activate', function(event) {
+  var setOfExpectedUrls = new Set(urlsToCacheKeys.values());
+
+  event.waitUntil(
+    caches.open(cacheName).then(function(cache) {
+      return cache.keys().then(function(existingRequests) {
+        return Promise.all(
+          existingRequests.map(function(existingRequest) {
+            if (!setOfExpectedUrls.has(existingRequest.url)) {
+              return cache.delete(existingRequest);
+            }
+          })
+        );
+      });
+    }).then(function() {
+      
+      return self.clients.claim();
+      
+    })
+  );
+});
+
+
+self.addEventListener('fetch', function(event) {
+  if (event.request.method === 'GET') {
+    // Should we call event.respondWith() inside this fetch event handler?
+    // This needs to be determined synchronously, which will give other fetch
+    // handlers a chance to handle the request if need be.
+    var shouldRespond;
+
+    // First, remove all the ignored parameters and hash fragment, and see if we
+    // have that URL in our cache. If so, great! shouldRespond will be true.
+    var url = stripIgnoredUrlParameters(event.request.url, ignoreUrlParametersMatching);
+    shouldRespond = urlsToCacheKeys.has(url);
+
+    // If shouldRespond is false, check again, this time with 'index.html'
+    // (or whatever the directoryIndex option is set to) at the end.
+    var directoryIndex = 'index.html';
+    if (!shouldRespond && directoryIndex) {
+      url = addDirectoryIndex(url, directoryIndex);
+      shouldRespond = urlsToCacheKeys.has(url);
+    }
+
+    // If shouldRespond is still false, check to see if this is a navigation
+    // request, and if so, whether the URL matches navigateFallbackWhitelist.
+    var navigateFallback = '/index.html';
+    if (!shouldRespond &&
+        navigateFallback &&
+        (event.request.mode === 'navigate') &&
+        isPathWhitelisted(["^(?!\\/__).*"], event.request.url)) {
+      url = new URL(navigateFallback, self.location).toString();
+      shouldRespond = urlsToCacheKeys.has(url);
+    }
+
+    // If shouldRespond was set to true at any point, then call
+    // event.respondWith(), using the appropriate cache key.
+    // console.log("[SW] fetch url " + url, shouldRespond);
+    if (shouldRespond) {
+      event.respondWith(
+        caches.open(cacheName).then(function(cache) {
+          return cache.match(urlsToCacheKeys.get(url)).then(function(response) {
+            if (response) {
+              let url = event.request.url;
+                // console.log("[SW] found cached url " + url);
+                /*if (url.indexOf(".wasm") > -1) {
+                  console.log("[SW] wasm not modified");
+                  let responseInit = {
+                    status: 304,
+                    statusText: 'Not Modified'
+                  };
+                  let notModifiedResponse = new Response('', responseInit);
+                  return notModifiedResponse;
+                }*/
+
+              return response;
+            }
+            throw Error('The cached response that was expected is missing.');
+          });
+        }).catch(function(e) {
+          // Fall back to just fetch()ing the request if some unexpected error
+          // prevented the cached response from being valid.
+          console.warn('Couldn\'t serve response for "%s" from cache: %O', event.request.url, e);
+          return fetch(event.request);
+        })
+      );
+    }
+  }
+});
+
+
+
+
+
+
+
+importScripts("./custom-service-worker.js");
